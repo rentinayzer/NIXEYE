18e98dfd731e4bbfa17085b5e64a113ba1f0a5ae
Maro Bazzite <rentinayzer@gmail.com>
2026-09-21 06:39:30 +0500

    Removed systemd.enable = true from hyprland due to it being an obsolete line

--- modules/de.nix
+++ modules/de.nix
@@ -6,7 +6,6 @@
   hm-maro.wayland.windowManager.hyprland = {
     enable = true;
     xwayland.enable = true;
-    systemd.enable = true;
 
     package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
     portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
